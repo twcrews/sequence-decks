db8839e44e8fcd74d260b608b325630928341c06
Tommy Crews <twcrews96@gmail.com>
2025-08-17 14:34:36 -0500

    Refactor background styles and add deck buttons.

--- Crews.Education.SequenceDecks/Models/Deck.cs
+++ Crews.Education.SequenceDecks/Models/Deck.cs
@@ -4,7 +4,8 @@ public record Deck
 {
 	public required string Name { get; init; }
 	public required string Color { get; init; }
-	public required IEnumerable<string> Content { get; init; }
+  public required string Slug { get; init; }
+  public required IEnumerable<string> Content { get; init; }
 
 	public IEnumerable<Card> GetCards() => Content
 		.Select(value => new Card() { Color = Color, Value = value });
