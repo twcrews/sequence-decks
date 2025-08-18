eb5ab589f4cb387f19b3ea20902ca90aa3b1c626
Tommy Crews <twcrews96@gmail.com>
2025-08-18 17:08:09 -0500

    Complete basic functionality.

--- Crews.Education.SequenceDecks/Services/AppStateService.cs
+++ Crews.Education.SequenceDecks/Services/AppStateService.cs
@@ -4,56 +4,16 @@ namespace Crews.Education.SequenceDecks.Services;
 
 public class AppStateService
 {
-	private bool _backgroundReady;
-	private bool _started;
-	private HashSet<string> _selectedDeckSlugs = new();
+	private bool _backgroundStarted;
 
-	public bool BackgroundReady 
-	{ 
-		get => _backgroundReady; 
-		set
-		{
-			_backgroundReady = value;
-			OnChange?.Invoke();
-		} 
-	}
+	public bool BackgroundStartRequested => _backgroundStarted;
 
-	public bool Started
-	{
-		get => _started;
-		set
-		{
-			_started = value;
-			OnChange?.Invoke();
-		}
-	}
-
-	public IReadOnlySet<string> SelectedDeckSlugs => _selectedDeckSlugs;
-
-	public bool IsViewingCards => Started && _selectedDeckSlugs.Count > 0;
-
-	public bool HasSelectedDecks => _selectedDeckSlugs.Count > 0;
-
-	public void ToggleDeckSelection(string deckSlug)
-	{
-		if (_selectedDeckSlugs.Contains(deckSlug))
-		{
-			_selectedDeckSlugs.Remove(deckSlug);
-		}
-		else
-		{
-			_selectedDeckSlugs.Add(deckSlug);
-		}
-		OnChange?.Invoke();
-	}
-
-	public bool IsDeckSelected(string deckSlug) => _selectedDeckSlugs.Contains(deckSlug);
-
-	public void ClearSelection()
-	{
-		_selectedDeckSlugs.Clear();
-		OnChange?.Invoke();
-	}
+  public void StartBackground()
+  {
+    if (_backgroundStarted) return;
+    _backgroundStarted = true;
+    OnChange?.Invoke();
+  }
 
   public event Action? OnChange;
 }
